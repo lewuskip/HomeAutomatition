78940ba6926f80533c7b38c389405554c5792ad9
Piotr Lewandowski <piotr.lewandowski@arm.com>
2017-10-24 18:55:58 +0100

    Added Theo switch

--- lights/hue_controller.py
+++ lights/hue_controller.py
@@ -2,7 +2,7 @@
 
 from phue import Bridge
 import time
-
+import bisect
 
 class HUEServer(object):
 
@@ -10,14 +10,54 @@ class HUEServer(object):
 
     bridgeIP = "192.168.1.103"
 
-    lightsDef = {
-            "Tymek" : 1,
-            "Theo"  : 2
+    COL_RELAX       = 447
+    COL_READ        = 346
+    COL_CONCENTRATE = 233
+    COL_ENERGIZE    = 156
+
+    BRI_RELAX       = 144
+    BRI_READ        = 254
+    BRI_CONCENTRATE = 254
+    BRI_ENERGIZE    = 254
+
+    lights = {
+            "Tymek" : {
+                "idx" : 1,
+                "status" : {},
+                "timeTable" : {
+                    0 : { "col" : 1, "value" : 0 },
+                    7 : { "col" : 1, "value" : 0 },
+                    20 : { "col" : 1, "value" : 0 },
+                    21 : { "col" : 1, "value" : 0 },
+                    22 : { "col" : 1, "value" : 0 },
+                    23 : { "col" : 1, "value" : 0 }
+                }
+            },
+
+            "Theo"  : {
+                "idx" : 2, ""
+                "status" : {},
+                "timetable" : {
+                    0 : { "col" : BRI_RELAX, "value" : 50 },
+                    7 : { "col" : BRI_ENERGIZE, "value" : 250 },
+                    17 : { "col" : BRI_RELAX, "value" : 255 },
+                    20 : { "col" : BRI_READ, "value" : 150 },
+                    21 : { "col" : BRI_RELAX, "value" : 100 },
+                    22 : { "col" : BRI_RELAX, "value" : 50 },
+                    23 : { "col" : BRI_RELAX, "value" : 25 }
+                }
+            }
     }
 
     sensorDef = {
-            "Tymek" : 2
+            "Tymek" : 2,
+            "Theo"  : 5
     }
+
+    BUTTON_ON = 1
+    BUTTON_DIM_UP = 2
+    BUTTON_DIM_DOWN = 3
+    BUTTON_OFF = 4
 
     def __init__(self):
         self._bridge = Bridge(self.bridgeIP)
@@ -26,10 +66,38 @@ class HUEServer(object):
         for name, idx in self.sensorDef.items():
             self.sensorsEvents[idx] = None
 
-    def lightEvent(self):
+        for name, idx in self.lights.items():
+            self.lights[name]["status"] = self._bridge.get_light(self.lights[name]["idx"])
+
+    def getCurrentColor(self, lightDef):
+        currHour = int(time.strftime("%H"))
+
+        keys = lightDef["timetable"].keys()
+        idx = bisect.bisect_right(keys, currHour) - 1
+
+        selectedDef = lightDef["timetable"][keys[idx]]
+        return selectedDef["col"], selectedDef["value"]
 
 
-        pass
+    def newEventTheo(self, event):
+
+        # get currentLightSTatus
+        self.lights["Theo"]["status"] = self._bridge.get_light(self.lights["Theo"]["idx"])
+
+        ct, bri = self.getCurrentColor(self.lights["Theo"])
+
+        buttonIdx = event / 1000
+
+        if buttonIdx==self.BUTTON_OFF and self.lights["Theo"]["status"]["state"]["on"] == True:
+            self._bridge.set_light(self.lights["Theo"]["idx"], "on", False)
+        elif buttonIdx==self.BUTTON_ON and self.lights["Theo"]["status"]["state"]["on"] == False:
+            print ("setting for Theo", ct, bri)
+            self._bridge.set_light(self.lights["Theo"]["idx"], "on", True)
+            self._bridge.set_light(self.lights["Theo"]["idx"], "ct", ct)
+            self._bridge.set_light(self.lights["Theo"]["idx"], "bri", bri)
+
+            print ( self._bridge.get_light(self.lights["Theo"]["idx"])["state"] )
+
 
     def decodeSensorEvents(self, idx, status):
         if self.sensorsEvents[idx] == None:
@@ -37,17 +105,15 @@ class HUEServer(object):
         else:
             if status["state"]["lastupdated"] != self.sensorsEvents[idx]["lastupdated"]:
                 self.sensorsEvents[idx] = status["state"]
-                print (status["state"])
+                self.newEventTheo( status["state"]["buttonevent"] )
 
 
     def start(self):
         while True:
-
             for name, idx in self.sensorDef.items():
                 status = self._bridge.get_sensor(idx)
                 self.decodeSensorEvents(idx, status)
-            time.sleep(0.2)
-            #print ( selfs._bridge.get_light(1) )
+            time.sleep(0.1)
 
 
 
